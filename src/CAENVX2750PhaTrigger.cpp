f532f7a498aae27e7e62b6bc1c8409dfdd38787f
rfoxkendo <rfoxkendo@gmail.com>
2022-05-13 06:16:50 -0400

    Wrote trigger object for single module

---
+++ src/CAENVX2750PhaTrigger.cpp
@@ -0,0 +1,44 @@
+/*
+*-------------------------------------------------------------
+ 
+ CAEN SpA 
+ Via Vetraia, 11 - 55049 - Viareggio ITALY
+ +390594388398 - www.caen.it
+
+------------------------------------------------------------
+
+**************************************************************************
+* @note TERMS OF USE:
+* This program is free software; you can redistribute it and/or modify it under
+* the terms of the GNU General Public License as published by the Free Software
+* Foundation. This program is distributed in the hope that it will be useful, 
+* but WITHOUT ANY WARRANTY; without even the implied warranty of 
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. The user relies on the 
+* software, documentation and results solely at his own risk.
+*
+* @file     CAENVX2750PhaTrigger.cpp
+* @brief    Implement the trigger class.
+* @author   Ron Fox
+*
+*/
+#include "CAENVX2750PhaTrigger.h
+#include "VX2750Pha.h"
+
+/**
+ * constructor:
+ *    @param module - the module we'll check for data.
+ */
+
+CAENVX2750PhaTrigger::CAENVX2750PhaTrigger(VX2750Pha& module) :
+    m_module(module)
+{}
+
+/**
+ * operator()
+ *    @return bool - true if the module has data to read.
+ */
+bool
+CAENVX2750PhaTrigger::operator()
+{
+    return m_module.hasData();
+}
---
+++ src/CAENVX2750PhaTrigger.h
@@ -0,0 +1,42 @@
+/*
+*-------------------------------------------------------------
+ 
+ CAEN SpA 
+ Via Vetraia, 11 - 55049 - Viareggio ITALY
+ +390594388398 - www.caen.it
+
+------------------------------------------------------------
+
+**************************************************************************
+* @note TERMS OF USE:
+* This program is free software; you can redistribute it and/or modify it under
+* the terms of the GNU General Public License as published by the Free Software
+* Foundation. This program is distributed in the hope that it will be useful, 
+* but WITHOUT ANY WARRANTY; without even the implied warranty of 
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. The user relies on the 
+* software, documentation and results solely at his own risk.
+*
+* @file     CAENVx2750PhaTrigger.h
+* @brief    Trigger for a single module
+* @author   Ron Fox
+*
+*/
+#ifndef CAENV2750PHATRIGGER_H
+#include <CEventTrigger.h>      // base class.
+
+class VX2750Pha;                // referenced class.
+
+/**
+ * @class CAENV2750PhaTrigger
+ *     Trigger class for NSCLDAQ and the
+ *     CAENV2x50 modules.
+ *     We contain the support class for the module we trigger.
+ */
+class  CAENVX2750PhaTrigger : public CEventTrigger
+{
+private:
+    VX2750Pha&    m_module;                      // Module we poll.
+public:
+    CAENVX2750PhaTrigger(VX2750Pha& module);
+    bool operator()();
+};
