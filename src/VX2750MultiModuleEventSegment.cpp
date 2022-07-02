a340f3b85edf4c645f5ded1d90638189cd312779
rfoxkendo <rfoxkendo@gmail.com>
2022-07-02 11:33:54 -0400

    Added multi module event segment so we can read the whole experiment.

---
+++ src/VX2750MultiModuleEventSegment.cpp
@@ -0,0 +1,122 @@
+/*
+*-------------------------------------------------------------
+ 
+ CAEN SpA 
+ Via Vetraia, 11 - 55049 - Viareggio ITALY
+ +390594388398 - www.caen.it
+
+------------------------------------------------------------
+
+**************************************************************************
+* @note TERMS OF USE:
+* This program is free software; you can redistribute it and/or modify it under
+* the terms of the GNU General Public License as published by the Free Software
+* Foundation. This program is distributed in the hope that it will be useful, 
+* but WITHOUT ANY WARRANTY; without even the implied warranty of 
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. The user relies on the 
+* software, documentation and results solely at his own risk.
+*
+* @file     VX2750MultiModuleEventSegment.cpp
+* @brief    Implement the event segment.
+* @author   Ron Fox
+*
+*/
+#include "VX2750MultiModuleEventSegment.h"
+
+#include "VX2750MultiTrigger.h"
+#include "VX2750EventSegment.h"
+#include <CExperiment.h>
+
+
+namespace caen_nscldaq {
+    /**
+     * constructor
+     * Our job is simple, just initialize the internal data from the
+     * parameters.
+     *    @param pExperment - pointer to the experiment object
+     *    @param pTrigger   - pointer to the trigger object.
+     */
+    VX2750MultiModuleEventSegment::VX2750MultiModuleEventSegment(
+        CExperiment* pExperiment, VX2750MultiTrigger* pTrigger
+    ) : m_pExperiment(pExperiment), m_pTrigger(pTrigger)
+    {}
+    
+    /**
+     * destructor
+     *   Ownership of our data remains with our clients:
+     */
+    VX2750MultiModuleEventSegment::~VX2750MultiModuleEventSegment()
+    {}
+    
+    /**
+     * initialize
+     *    Just get the module pointers from the trigger, interate over
+     *    them and initialize each one.
+     *      This implies, for a synchronized set, the order is important with
+     *      the master module needing to be last in the list.
+     */
+    void
+    VX2750MultiModuleEventSegment::initialize()
+    {
+        auto modules = m_pTrigger->getModules();
+        for (auto p : modules) {
+            p->initialize();
+        }
+    }
+    /**
+     * disable
+     *    As above but call disable for each item:
+     */
+    void VX2750MultiModuleEventSegment::disable()
+    {
+        auto modules = m_pTrigger->getModules();
+        for (auto p : modules) {
+            p->disable();
+        }
+    }
+    /**
+     * onPause
+     *    This is just disable:
+     */
+    void VX2750MultiModuleEventSegment::onPause()
+    {
+        disable();
+    }
+    /**
+     * onResume:
+     *    Similarly, this is is just enable.
+     */
+    void  VX2750MultiModuleEventSegment::onResume()
+    {
+        initialize();
+    }
+    /**
+     * read:
+     *    While the set of triggered modules is not empty, get the last triggered
+     *    module, read it, remove it from the trigger set and
+     *    if the result is a nonempty trigger list, retrigger.
+     * @param pBuffer - pointer to where the data will be stired,
+     * @param maxwords - Maximum number of words that can be stored.
+     * @return size_t - number of words read into pBuffr (word == uint16_t).
+     */
+    size_t
+    VX2750MultiModuleEventSegment::read(void* pBuffer, size_t maxwords)
+    {
+        size_t nRead = 0;
+        std::vector<VX2750EventSegment*>& triggered = m_pTrigger->getTriggeredModules();
+        if (!triggered.empty()) {
+            // Defensive.
+            
+            VX2750EventSegment* pSeg = triggered.back();
+            size_t nRead = pSeg->read(pBuffer, maxwords);
+            triggered.pop_back();
+            
+            if (!triggered.empty()) {
+                m_pExperiment->haveMore();
+            }
+            
+        }
+        return nRead;
+    }
+    
+}
---
+++ src/VX2750MultiModuleEventSegment.h
@@ -0,0 +1,61 @@
+/*
+*-------------------------------------------------------------
+ 
+ CAEN SpA 
+ Via Vetraia, 11 - 55049 - Viareggio ITALY
+ +390594388398 - www.caen.it
+
+------------------------------------------------------------
+
+**************************************************************************
+* @note TERMS OF USE:
+* This program is free software; you can redistribute it and/or modify it under
+* the terms of the GNU General Public License as published by the Free Software
+* Foundation. This program is distributed in the hope that it will be useful, 
+* but WITHOUT ANY WARRANTY; without even the implied warranty of 
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. The user relies on the 
+* software, documentation and results solely at his own risk.
+*
+* @file     VX2750MultiModuleEventSegment.h
+* @brief    Event segment with several VX2750EventSegments as elements.
+* @author   Ron Fox
+*
+*/
+#ifndef VX2750MULTIMODULEEVENTSEGMENT_H
+#define VX2750MULTIMODULEEVENTSEGMENT_H
+
+#include <CEventSegment.h>
+
+class CExperiment;
+namespace caen_nscldaq {
+    // Forward class references:
+    
+    class VX2750MultiTrigger;
+    
+    /**
+     * @class VX2750MultiModuleEventSegment
+     *    Event segment that handles several modules of VX27x0 digitizers.
+     *    Note that we assume triggering is being handled by a multitrigger
+     *    object and we can use its concept of the triggered modules to actually
+     *    do the readout.  We'll get called from the trigger loop and read
+     *    _all_ the triggered modules. each as one event using the retrigger
+     *    flag in the event segment by calling the experiment's haveMore
+     *    until we're done.
+     */
+    class VX2750MultiModuleEventSegment : public CEventSegment {
+    private:
+        CExperiment*        m_pExperiment;
+        VX2750MultiTrigger* m_pTrigger;
+    public:
+        VX2750MultiModuleEventSegment(CExperiment* pExperiment, VX2750MultiTrigger* pTrigger);
+        virtual ~VX2750MultiModuleEventSegment();
+        
+        void initialize();
+        void disable();
+        void onPause();
+        void onResume();
+        size_t read(void* pBuffer, size_t maxwords);
+    };
+}                         // caen_nscldaq namespace.
+
+#endif
