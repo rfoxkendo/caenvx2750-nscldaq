20ddbf1825e4d33959d1b9a99862de656394f120
rfoxkendo <rfoxkendo@gmail.com>
2022-08-06 16:20:37 -0400

    Provide mechanisms to target tests to a module

--- src/TestRunner.cpp
+++ src/TestRunner.cpp
@@ -2,10 +2,39 @@
 #include <cppunit/ui/text/TextTestRunner.h>
 #include <string>
 #include <iostream>
+#include <cstdlib>
+
 using namespace std;
+void usage(const char* name)
+{
+  std::cerr<< "Usage:\n";
+  std::cerr << "  " << name << "  pid-or-ip 0|1\n";
+  std::cerr <<"Where:\n";
+  std::cerr << "    pid-or-ip is either the USB PID or ip address to use\n";
+  std::cerr << "    0 - for ethernt (IP address needed) or 1 - for USB (PID needed)\n";
+  
+}
+
+std::string connection;
+bool        isUsb;
 
 int main(int argc, char** argv)
 {
+  // Pull out the connection parameters which are either
+  //  PID 1
+  // or
+  //  ip.dotted.addr.ees 0
+  //
+  if (argc !=  3) {
+    usage(argv[0]);
+  }
+  connection = argv[1];
+  int usb    = std::atoi(argv[2]);
+  isUsb      = usb ? true : false;
+  
+  std::cout << "Connecting via " << (isUsb ? "USB" : "ETHERNET") << " to " << connection
+    << std::endl;
+  
   CppUnit::TextTestRunner
                runner; // Control tests.
   CppUnit::TestFactoryRegistry&
