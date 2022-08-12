bb27d867d1574870b0a0bb49f8f5f6eadeb17a45
rfoxkendo <rfoxkendo@gmail.com>
2022-08-12 10:52:55 -0400

    *   Fix up Makefile so that it properly handles JSON requirements     for tests. *   Some initial tests of VX2750Pha

--- src/vx2750phatests.cpp
+++ src/vx2750phatests.cpp
@@ -15,28 +15,70 @@
  */
 #include <cppunit/extensions/HelperMacros.h>
 #include <cppunit/Asserter.h>
-#include "Asserts.h"
+#include <string>
 
-class aTestSuite : public CppUnit::TestFixture {
-    CPPUNIT_TEST_SUITE(aTestSuite);
-    CPPUNIT_TEST(test_1);
+#include "Asserts.h"
+#include "VX2750Pha.h"
+extern std::string connection;
+extern bool        isUsb;
+
+using namespace caen_nscldaq;
+
+class vx2750phatest : public CppUnit::TestFixture {
+    CPPUNIT_TEST_SUITE(vx2750phatest);
+    CPPUNIT_TEST(getcup);
+    CPPUNIT_TEST(getfpgafw);
+    CPPUNIT_TEST(getfwType);
+    CPPUNIT_TEST(getmodelCode);
+    CPPUNIT_TEST(getpiggycode);
+    CPPUNIT_TEST(getmodel);
     CPPUNIT_TEST_SUITE_END();
     
 private:
-
+    VX2750Pha* m_pModule;
 public:
     void setUp() {
-        
+        m_pModule = new VX2750Pha(connection.c_str(), isUsb);
     }
     void tearDown() {
-        
+        delete m_pModule;
     }
 protected:
-    void test_1();
+    void getcup();
+    void getfpgafw();
+    void getfwType();
+    void getmodelCode();
+    void getpiggycode();
+    void getmodel();
 };
 
-CPPUNIT_TEST_SUITE_REGISTRATION(aTestSuite);
+CPPUNIT_TEST_SUITE_REGISTRATION(vx2750phatest);
 
-void aTestSuite::test_1()
+void vx2750phatest::getcup()
 {
+    CPPUNIT_ASSERT_NO_THROW(m_pModule->getCupVersion());
+}
+void vx2750phatest::getfpgafw()
+{
+    CPPUNIT_ASSERT_NO_THROW(m_pModule->getFpgaFWVersion());
+}
+void vx2750phatest::getfwType()
+{
+    VX2750Pha::FwType type;
+    CPPUNIT_ASSERT_NO_THROW(type = m_pModule->getFirmwareType());
+    ASSERT(type == VX2750Pha::DPP_PHA);
+}
+void vx2750phatest::getmodelCode()
+{
+    CPPUNIT_ASSERT_NO_THROW(m_pModule->getModelCode());
+}
+void vx2750phatest::getpiggycode()
+{
+    
+    CPPUNIT_ASSERT_NO_THROW( m_pModule->getPiggyBackCode());
+    
+}
+void vx2750phatest::getmodel()
+{
+    CPPUNIT_ASSERT_NO_THROW(m_pModule->getModelName());
 }
