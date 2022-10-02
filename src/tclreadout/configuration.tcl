398cf17966ecb12459d88c0759975fa88c1d5621
rfoxkendo <rfoxkendo@gmail.com>
2022-10-02 08:48:42 -0400

    *  Turn off probes. *   Add CAEN Copyright notice.

--- src/tclreadout/configuration.tcl
+++ src/tclreadout/configuration.tcl
@@ -1,3 +1,27 @@
+#/*
+#*-------------------------------------------------------------
+# 
+# CAEN SpA 
+# Via Vetraia, 11 - 55049 - Viareggio ITALY
+# +390594388398 - www.caen.it
+#
+#------------------------------------------------------------
+#
+#**************************************************************************
+#* @note TERMS OF USE:
+#* This program is free software; you can redistribute it and/or modify it under
+#* the terms of the GNU General Public License as published by the Free Software
+#* Foundation. This program is distributed in the hope that it will be useful, 
+#* but WITHOUT ANY WARRANTY; without even the implied warranty of 
+#* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. The user relies on the 
+#* software, documentation and results solely at his own risk.
+#*
+#* @file      configuration.tcl
+#* @brief     Sample configuration file for tclreadout demo.
+#* @author   Ron Fox
+#*
+#*/
+
 v27xxpha create adc1
 
 # Configure the adc to read from the pulser:
@@ -8,7 +32,7 @@ v27xxpha create adc1
 v27xxpha config adc1 gbltriggersrc TestPulse \
     wavetriggersrc [lrepeat 64 GlobalTriggerSource] \
     eventtriggersrc [lrepeat 64 GlobalTriggerSource] \
-    testpulseperiod 100000000 testpulsewidth 32
+    testpulseperiod 100000000 testpulsewidth 100
 
 v27xxpha config adc1 testpulselowlevel 0x0 testpulsehighlevel 0x2000
 
@@ -41,7 +65,8 @@ v27xxpha config adc1 eflflimitation [lrepeat 64 false] \
 
 # Put analog probe 1 in the event.
 
-v27xxpha config adc1 readanalogprobes [list off off];   
+v27xxpha config adc1 readanalogprobes [list off off];
+v27xxpha config adc1 readdigitalprobes [list off off off off]
     
 
 
